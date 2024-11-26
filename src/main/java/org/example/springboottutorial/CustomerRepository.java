5a008f1557b45eb160b60d00229f1c39d0dfe659
zhangcongcong <66811734+sorryzcc@users.noreply.github.com>
2024-11-26 16:32:57 +0800

    Tutorial:Explore spring support features

---
+++ src/main/java/org/example/springboottutorial/CustomerRepository.java
@@ -0,0 +1,8 @@
+package org.example.springboottutorial;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CustomerRepository extends CrudRepository<Customer, Integer> {
+
+    Customer findCustomerById(Integer id);
+}
