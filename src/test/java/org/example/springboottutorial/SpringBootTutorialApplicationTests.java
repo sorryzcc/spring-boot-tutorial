04012716f048638ecc7660a09bff38190efb850a
zhangcongcong <66811734+sorryzcc@users.noreply.github.com>
2024-11-26 14:15:53 +0800

    第一次提交

---
+++ src/test/java/org/example/springboottutorial/SpringBootTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springboottutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTutorialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
